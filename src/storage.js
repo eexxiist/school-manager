32e93041c88d1918d91b4553454e9c97395650e6
eexxiist <mego_dilya@mail.ru>
2026-05-09 15:35:27 +0300

    add file storage

--- src/storage.js
+++ src/storage.js
@@ -4,17 +4,21 @@ const TRAINEE_DATA_FILE_PATH = './data/trainees.json';
 const COURSE_DATA_FILE_PATH = './data/courses.json';
 
 export function loadTraineeData() {
-  // Use the fs module to read the trainees.json file and return the data as a JavaScript object  
+  const data = fs.readFileSync(TRAINEE_DATA_FILE_PATH, 'utf-8');
+  return JSON.parse(data);
 }
 
-export function saveTraineeData() {
-  // Use the fs module to write the updated trainee data back to the trainees.json file 
+export function saveTraineeData(data) {
+  const dataSave = JSON.stringify(data, null, 2);
+  fs.writeFileSync(TRAINEE_DATA_FILE_PATH, dataSave, 'utf-8');
 }
 
 export function loadCourseData() {
-  // TODO: Implement
+  const data = fs.readFileSync(COURSE_DATA_FILE_PATH, 'utf-8');
+  return JSON.parse(data);
 }
 
-export function saveCourseData() {
-  // TODO: Implement
+export function saveCourseData(data) {
+  const dataSave = JSON.stringify(data, null, 2);
+  fs.writeFileSync(COURSE_DATA_FILE_PATH, dataSave, 'utf-8');
 }
